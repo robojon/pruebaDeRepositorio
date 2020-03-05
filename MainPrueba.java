55acb997d19d2375f5c7872403a554f022e16b58
Jonathan Crespo <jonathan.crespo@urjc.es>
2020-03-05 17:34:54 +0100

    hecho saludo

--- MainPrueba.java
+++ MainPrueba.java
@@ -1,8 +1,13 @@
 
 public class MainPrueba {
+	
+	public static String saluda(String nombre) {
+		return "Soy "+nombre;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo nuevo");
+		System.out.println(saluda("Jonathan"));
 
 	}
 
